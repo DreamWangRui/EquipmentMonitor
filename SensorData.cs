ca2914e80e4e6d8b98654e4e96293ee3acc8462a
Dream <193394639@qq.com>
2026-04-08 10:57:15 +0800

    初始提交：WPF设备监控系统基础框架

---
+++ SensorData.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EquipmentMonitor
+{
+    public class SensorData
+    {
+        public DateTime Time { get; set; }
+        public double Value { get; set; }
+
+        public SensorData(double value)
+        {
+            Time = DateTime.Now;
+            Value = value;
+        }
+    }
+}
